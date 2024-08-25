e422cdd3538afd62d2cc73bed24c7cdd2c2fdfc5
valfis6 <valfis720@gmail.com>
2024-08-25 19:09:40 +0100

    Update timer.py

--- src/modlunky2/ui/trackers/timer.py
+++ src/modlunky2/ui/trackers/timer.py
@@ -29,6 +29,50 @@ class IL:
     theme: int
     time: int
 
+class COTimers():
+    def __init__(self):
+        self.last_processed_lvl = (0, 0)
+        self.co_entry = 0
+        self.co_avg = 0
+        self.co_final = 0
+
+    def update(self, proc: Spel2Process):
+        game_state = proc.get_state()
+        if game_state is None:
+            return None
+
+        # Get accurate world + level for calculations
+        if game_state.screen == 13:
+            world = game_state.world_next
+            level = game_state.level_next
+        elif game_state.screen == 12:
+            world = game_state.world
+            level = game_state.level
+        else:
+            return
+        
+        # We don't wanna reprocess multiple times in the same level
+        if (world, level) == self.last_processed_lvl:
+            return
+        self.last_processed_lvl = (world, level)
+        
+        # Update entry time
+        if world == 8 and level == 5:
+            self.co_entry = game_state.time_total
+        elif world < 8:
+            self.co_entry = 0
+
+        # Update average time and predicted final time
+        if world == 8 and level > 5:
+            self.co_avg = ( game_state.time_total - self.co_entry ) / ( level - 5 )
+            self.co_final = game_state.time_total + ( 99 - level ) * self.co_avg
+        else:
+            self.co_avg = 0
+            self.co_final = 0
+
+    def get(self, proc: Spel2Process):
+        self.update(proc)
+        return self.co_entry, self.co_avg, self.co_final
 
 class TimerModifiers(ttk.LabelFrame):
     def __init__(
@@ -111,6 +155,18 @@ class TimerModifiers(ttk.LabelFrame):
         )
         self.show_ils_checkbox.grid(row=0, column=6, pady=5, padx=5, sticky="w")
 
+        self.show_co_timers = tk.BooleanVar()
+        self.show_co_timers.set(self.timer_tracker_config.show_co_timers)
+        self.show_co_timers_checkbox = ttk.Checkbutton(
+            self,
+            text="CO",
+            variable=self.show_co_timers,
+            onvalue=True,
+            offvalue=False,
+            command=self.toggle_show_co_timers,
+        )
+        self.show_co_timers_checkbox.grid(row=0, column=7, pady=5, padx=5, sticky="w")
+
     def toggle_show_total(self):
         self.timer_tracker_config.show_total = self.show_total.get()
         self.parent.config_update_callback()
@@ -133,6 +189,10 @@ class TimerModifiers(ttk.LabelFrame):
 
     def toggle_show_ils(self):
         self.timer_tracker_config.show_ils = self.show_ils.get()
+        self.parent.config_update_callback()
+
+    def toggle_show_co_timers(self):
+        self.timer_tracker_config.show_co_timers = self.show_co_timers.get()
         self.parent.config_update_callback()
 
 
@@ -206,6 +266,7 @@ class TimerTracker(Tracker[TimerTrackerConfig, WindowData]):
         self.time_startup = 0
         self.il_times = []
         self.first_level = None
+        self.co_timers = COTimers()
 
     def initialize(self):
         self.time_total = 0
@@ -217,6 +278,7 @@ class TimerTracker(Tracker[TimerTrackerConfig, WindowData]):
         self.time_startup = 0
         self.il_times = []
         self.first_level = None
+        self.co_timers = COTimers()
 
     def poll(self, proc: Spel2Process, config: TimerTrackerConfig) -> WindowData:
         game_state = proc.get_state()
@@ -230,6 +292,7 @@ class TimerTracker(Tracker[TimerTrackerConfig, WindowData]):
         self.time_last_level = game_state.time_last_level
         self.time_tutorial = game_state.time_tutorial
         self.time_session = self.time_startup + (time.time() - self.time_start) * 60
+        self.co_entry, self.co_avg, self.co_final = self.co_timers.get(proc)
 
         if self.first_level is None or game_state.level_count == 0:
             self.first_level = game_state.level_count
@@ -270,7 +333,10 @@ class TimerTracker(Tracker[TimerTrackerConfig, WindowData]):
             out.append(f"Tutorial: {self.format(self.time_tutorial)}")
         if config.show_session:
             out.append(f"Session: {self.format(self.time_session)}")
-
+        if config.show_co_timers:
+            out.append(f"CO entry: {self.format(self.co_entry)}")
+            out.append(f"CO average: {self.format(self.co_avg)}")
+            out.append(f"CO final: {self.format(self.co_final)}")
         if config.show_ils:
             if self.first_level == 0:
                 for il in self.il_times:
@@ -279,3 +345,4 @@ class TimerTracker(Tracker[TimerTrackerConfig, WindowData]):
                 out.append("Reset run to track ILs")
 
         return "\n".join(out)
+    
